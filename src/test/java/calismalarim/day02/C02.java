cdca117579900c009f78e1c0773b6dabf3894c80
isminizsibel <email_adresinizsibelinko33@gmail.com>
2023-03-23 14:53:42 +0300

    v3

--- src/test/java/calismalarim/day02/C02.java
+++ src/test/java/calismalarim/day02/C02.java
@@ -1,10 +1,14 @@
 package calismalarim.day02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C02 {
     public static void main(String[] args) {
@@ -14,5 +18,35 @@ public class C02 {
         driver.manage().window().maximize();
         //     Tüm sayfa için maksimum bekleme süresi olarak 15 saniye belirleyin
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)) ;
+        /*
+     Amazon sayfasına gidin
+     Search kutusunu locate edin
+     Search kutusunda iphone aratın
+     Sayfadaki <input> ve <a> tag'larinin sayısını yazdırın
+     Sayfayı kapatınız
+     */
+
+
+   //  Amazon sayfasına gidin
+
+        driver.get("https://www.amazon.com");
+
+
+
+//         Search kutusunu locate edin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+
+//         Arama kutusunda iphone aratın
+        aramaKutusu.sendKeys("iphone"+ Keys.ENTER);
+
+
+//     Sayfadaki <input> ve <a> tag'larinin sayısını yazdırın
+        List<WebElement>inputList=driver.findElements(By.tagName("input"));
+        System.out.println(inputList.size());
+List<WebElement>aList=driver.findElements(By.tagName("a"));
+        System.out.println(aList.size());
+//     Sayfayı kapatınız
+driver.close();
+
     }
 }
